e799d42f9acfee1927263084494b547e115fbad1
Luc Russell <luc.russell@virtustream.com>
2017-09-12 11:42:32 -0400

    Correct typo in on_get

--- tinyblockchain/resources/blocks.py
+++ tinyblockchain/resources/blocks.py
@@ -5,7 +5,7 @@ class BlocksResource(object):
     def __init__(self, state):
         self.state = state
 
-    def on_get(self, req, resp, ** params):
+    def on_get(self, req, resp, **params):
         print("BlocksResource called")
         chain_to_send = []
         # Return chain as dictionary
